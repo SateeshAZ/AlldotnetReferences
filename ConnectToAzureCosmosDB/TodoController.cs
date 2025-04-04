3fa6848332afc72469b8c394eeb6661a6187699e
SateeshAZ <sateesh.gundeboina@livingstone-tech.com>
2025-04-04 12:32:24 +0530

    label change

--- ConnectToAzureCosmosDB/TodoController.cs
+++ ConnectToAzureCosmosDB/TodoController.cs
@@ -18,7 +18,7 @@ namespace FunctionAppWithCosmosDB.Controllers
             _cosmosDBService = cosmosDBService;
         }
 
-        [Function("GetAllTodos")]
+        [Function("GetAllTodoss")]
         public async Task<IActionResult> GetTodosAsync(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "todos")] HttpRequest req,
             ILogger log)
